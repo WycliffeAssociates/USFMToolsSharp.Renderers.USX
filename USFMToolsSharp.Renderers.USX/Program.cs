018edf6783f926b377bc19eb63c59431d9f82373
Thundelly <thundelly0609@gmail.com>
2021-09-09 05:18:51 -0700

    Initialized Program

---
+++ USFMToolsSharp.Renderers.USX/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace USFMToolsSharp.Renderers.USX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
