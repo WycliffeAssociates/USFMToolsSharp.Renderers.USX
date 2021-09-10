d5fb079c34812d1dfa7c55a8665120b08cb4cdcd
Thundelly <thundelly0609@gmail.com>
2021-09-10 03:36:57 -0700

    Adding Markers WIP

--- USFMToolsSharp.Renderers.USX/USXConfig.cs
+++ USFMToolsSharp.Renderers.USX/USXConfig.cs
@@ -3,10 +3,12 @@ namespace USFMToolsSharp.Renderers.USX
     public class USXConfig
     {
         public bool PartialUSX;
+        public string USXVersion;
 
         public USXConfig()
         {
             PartialUSX = false;
+            USXVersion = "3.0";
         }
 
         public USXConfig(bool partialUSX)
--- USFMToolsSharp.Renderers.USX/USXRenderer.cs
+++ USFMToolsSharp.Renderers.USX/USXRenderer.cs
@@ -55,7 +55,7 @@ namespace USFMToolsSharp.Renderers.USX
 
             return null;
         }
-
+        
         private string RenderMarker(Marker input)
         {
             var output = new StringBuilder();
@@ -63,20 +63,80 @@ namespace USFMToolsSharp.Renderers.USX
             switch (input)
             {
                 case IDMarker idMarker:
-                    string code = idMarker.TextIdentifier.Substring(0, 3);
-                    var bibleName = idMarker.TextIdentifier.Substring(4);
+                    var bookCode = idMarker.TextIdentifier.Substring(0, 3);
+                    var bibleVersion = idMarker.TextIdentifier.Substring(4);
                     
-                    output.Append($"<book style=\"{input.Identifier}\" code=\"{code}\">");
-                    output.Append(bibleName);
+                    output.Append($"<book style=\"{input.Identifier}\" code=\"{bookCode}\">");
+                    output.Append(bibleVersion);
                     output.AppendLine("</book>");
                     break;
-
                 
+                case USFMMarker usfmMarker:
+                    output.AppendLine($"<para style=\"{usfmMarker.Identifier}\">{usfmMarker.Version}</para>");
+                    break;
+                
+                case IDEMarker ideMarker:
+                    output.AppendLine($"<para style=\"{ideMarker.Identifier}\">{ideMarker.Encoding}</para>");
+                    break;
+                    
+                case HMarker hMarker:
+                    output.AppendLine($"<para style=\"{hMarker.Identifier}\">{hMarker.HeaderText}</para>");
+                    break;
+                
+                case TOC1Marker toc1Marker:
+                    output.AppendLine($"<para style=\"{toc1Marker.Identifier}\">{toc1Marker.LongTableOfContentsText}</para>");
+                    break;
+
+                case TOC2Marker toc2Marker:
+                    output.AppendLine($"<para style=\"{toc2Marker.Identifier}\">{toc2Marker.ShortTableOfContentsText}</para>");
+                    break;
+                
+                case TOC3Marker toc3Marker:
+                    output.AppendLine($"<para style=\"{toc3Marker.Identifier}\">{toc3Marker.BookAbbreviation}</para>");
+                    break;
+                
+                case MTMarker mtMarker:
+                    output.AppendLine($"<para style=\"{mtMarker.Identifier}\">{mtMarker.Title}</para>");
+                    break;
+                
+                case SMarker sMarker:
+                    output.AppendLine($"<para style=\"{sMarker.Identifier}{sMarker.Weight}\">{sMarker.Text}</para>");
+                    break;
+                
+                case PMarker pMarker:
+                    output.AppendLine($"<para style=\"{pMarker.Identifier}\">");
+
+                    foreach (Marker marker in input.Contents)
+                    {
+                        output.Append(RenderMarker(marker));
+                    }
+
+                    output.AppendLine("</para>");
+                    break;
+                
+                case CMarker cMarker:
+                    break;
+                
+                case VMarker vMarker:
+                    output.AppendLine($"<verse style=\"{vMarker.Identifier}\" " +
+                                      $"number=\"{vMarker.VerseNumber}\" " +
+                                      $"sid=\":{vMarker.VerseNumber}\" />");
+                    foreach (Marker marker in input.Contents)
+                    {
+                        output.Append(RenderMarker(marker));
+                    }
+                    output.AppendLine("<verse/>");
+                    break;
+                
+                case TextBlock textBlock:
+                    output.AppendLine(textBlock.Text.Trim());
+                    break;
+
                 default:
                     UnrenderableTags.Add(input.Identifier);
                     break;
             }
-            
+
             return output.ToString();
         }
     }
