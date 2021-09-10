76900ff5e54f88cf08398aca6bc6985135b15c21
Eric Ko <thundelly0609@gmail.com>
2021-09-10 12:00:23 -0700

    Changed version from string to double

--- USFMToolsSharp.Renderers.USX/USXConfig.cs
+++ USFMToolsSharp.Renderers.USX/USXConfig.cs
@@ -3,15 +3,15 @@ namespace USFMToolsSharp.Renderers.USX
     public class USXConfig
     {
         public bool PartialUSX;
-        public string USXVersion;
+        public double USXVersion;
 
         public USXConfig()
         {
             PartialUSX = false;
-            USXVersion = "3.0";
+            USXVersion = 3.0;
         }
 
-        public USXConfig(bool partialUSX, string USXVersion)
+        public USXConfig(bool partialUSX, double USXVersion)
         {
             PartialUSX = partialUSX;
             this.USXVersion = USXVersion;
