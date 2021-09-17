e64f2b7f781de13c1da502b664fde14b6da310a8
Thundelly <thundelly0609@gmail.com>
2021-09-16 23:45:31 -0700

    Initiated test proj

---
+++ USFMToolsSharp.Renderers.USX.Tests/USXRenderTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+ 
+namespace USFMToolsSharp.Renderers.USX.Tests
+{
+    [TestClass]
+    public class USXRenderTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+
+    }
+}
